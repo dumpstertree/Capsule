9d5484d62ed4fb0a7cd0e45eea0778c3edac7f32
dumpstertree <dumpstertree@gmail.com>
2026-04-16 16:41:43 -0400

    Create preconfig-sunshine.sh

---
+++ preconfig-sunshine.sh
@@ -0,0 +1,20 @@
+# --- Helpers ---
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+YELLOW='\033[1;33m'
+BLUE='\033[0;34m'
+NC='\033[0m'
+
+log()   { echo -e "${GREEN}[SETUP]${NC} $1"; }
+warn()  { echo -e "${YELLOW}[WARN]${NC} $1"; }
+error() { echo -e "${RED}[ERROR]${NC} $1"; exit 1; }
+info()  { echo -e "${BLUE}[INFO]${NC} $1"; }
+
+# ---  ---
+log "Generating sunshine.conf..."
+touch /gamer/.config/sunshine.conf
+printf 'origin_web_ui_allowed = wan\naddress_family = both\nupnp = disabled' >> /etc/pacman.conf
+
+# --- Setup Creds ---
+log "Setting Sunshine Credentials..."
+sunshine --creds gamer gamer
