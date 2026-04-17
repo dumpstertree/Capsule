e0e82dec023d42c2186d8f7f649f8288996697e1
dumpstertree <dumpstertree@gmail.com>
2026-04-16 21:05:06 -0400

    Update start-stream.sh

--- start-stream.sh
+++ start-stream.sh
@@ -4,15 +4,12 @@
 killall -q sunshine
 killall -q Xorg
 
+mount -o remount,rw /sys
+
+/lib/systemd/systemd-udevd --daemon
+
 # start dummy Xorg on :0 this needs to not be hardcoded
-SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
-echo "Running from: $SCRIPT_DIR"
-
-cd "$SCRIPT_DIR" || exit 1
-
-ls -l xorg.conf || { echo "xorg.conf missing"; exit 1; }
-
-Xorg :0 -config ./xorg.conf &
+Xorg :0 -config ./xorg.conf -noreset -novtswitch &
 sleep 2
 
 # start dbus session and run everything inside it
