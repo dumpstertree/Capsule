0d1e3b1cd18618218a5748189f27017619b36814
dumpstertree <dumpstertree@gmail.com>
2026-04-19 12:37:03 -0400

    Update preconfig-xorg.sh

--- preconfig-xorg.sh
+++ preconfig-xorg.sh
@@ -1,2 +1,5 @@
 echo "Coping a Xorg.conf into proper folder"
 cp /Capsule/example-xorg.conf /etc/X11/xorg.conf
+
+echo "Coping a Xwrapper.config into proper folder"
+cp /Capsule/example-Xwrapper.config /etc/X11/Xwrapper.config
