d942ae4453d58f687c41c3b2fb143fa881bf9138
dumpstertree <dumpstertree@gmail.com>
2026-04-19 12:21:45 -0400

    Update preconfig-capsule.sh

--- preconfig-capsule.sh
+++ preconfig-capsule.sh
@@ -2,13 +2,26 @@
 chmod -R 777 /Capsule
 
 # copy example service into folder
-cp /Capsule/example-capsule.service /etc/systemd/system/capsule.service
+#cp /Capsule/example-capsule.service /etc/systemd/system/capsule.service
 
 # update the daemon list with newly created 
-systemctl daemon-reload
+#systemctl daemon-reload
 
 # enable the service for next time
-systemctl enable capsule.service
+#systemctl enable capsule.service
 
 # start the service now
-systemctl start capsule.service
+#systemctl start capsule.service
+
+# create the service target directory
+mkdir -p /home/gamer/.config/systemd/user/default.target.wants
+chown -R gamer:gamer /home/gamer/.config/systemd
+
+#copy example service to target
+cp /Capsule/example-capsule.service /usr/lib/systemd/user/capsule.service
+
+#reload daemons before start
+runuser -u gamer -- systemctl --user daemon-reload
+
+# enable now and in the future
+runuser -u gamer -- systemctl enable --now capsule.service
