fce602f87a98dc1a2e938de230a71bd7c448f11e
dumpstertree <dumpstertree@gmail.com>
2026-04-18 20:47:08 -0400

    Create preconfigure-capsule.sh

---
+++ preconfigure-capsule.sh
@@ -0,0 +1,11 @@
+# copy example service into folder
+cp /Capsule/example-capsule.service /etc/systemd/system/capsule.service
+
+# update the daemon list with newly created 
+systemctl daemon-reload
+
+# enable the service for next time
+systemctl enable capsule.service
+
+# start the service now
+systemctl start capsule.service
