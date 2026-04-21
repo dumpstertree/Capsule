573aea8cf1c0a60de153bd40eb04a35355b9de51
dumpstertree <dumpstertree@gmail.com>
2026-04-20 21:25:55 -0400

    Update preconfig-audio.sh

--- preconfig-audio.sh
+++ preconfig-audio.sh
@@ -1,6 +1,6 @@
 # enable all audio servicess
-systemctl --user enable pipewire.service
-systemctl --user enable wireplumber.service
-systemctl --user enable pipewire-pulse.service
+runuser -u gamer -- systemctl --user enable pipewire.service
+runuser -u gamer -- systemctl --user enable wireplumber.service
+runuser -u gamer -- systemctl --user enable pipewire-pulse.service
 #systemctl --user enable pipewire-pulse.socket
 #systemctl --user enable pipewire.socket
