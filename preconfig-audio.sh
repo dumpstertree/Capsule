300f1ea0dfce07c176fd9d4547d5aa5fec508d16
dumpstertree <dumpstertree@gmail.com>
2026-04-22 07:11:51 -0400

    Update preconfig-audio.sh

--- preconfig-audio.sh
+++ preconfig-audio.sh
@@ -6,10 +6,7 @@
 #systemctl --user enable pipewire.socket
 # As root inside the container
 mkdir -p /home/gamer/.config/systemd/user/default.target.wants
-ln -sf /usr/lib/systemd/user/pipewire.service \
-    /home/gamer/.config/systemd/user/default.target.wants/pipewire.service
-ln -sf /usr/lib/systemd/user/pipewire-pulse.service \
-    /home/gamer/.config/systemd/user/default.target.wants/pipewire-pulse.service
-ln -sf /usr/lib/systemd/user/wireplumber.service \
-    /home/gamer/.config/systemd/user/default.target.wants/wireplumber.service
+ln -sf /usr/lib/systemd/user/pipewire.service /home/gamer/.config/systemd/user/default.target.wants/pipewire.service
+ln -sf /usr/lib/systemd/user/pipewire-pulse.service /home/gamer/.config/systemd/user/default.target.wants/pipewire-pulse.service
+ln -sf /usr/lib/systemd/user/wireplumber.service /home/gamer/.config/systemd/user/default.target.wants/wireplumber.service
 chown -R gamer:gamer /home/gamer/.config
