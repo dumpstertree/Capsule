58a13fa003a8888ac1122b448b7cf20b20715bd4
dumpstertree <dumpstertree@gmail.com>
2026-04-21 20:41:57 -0400

    Update open_capsule.sh

--- open_capsule.sh
+++ open_capsule.sh
@@ -7,14 +7,14 @@ sh /Capsule/install_dependencies2.sh
 echo "Setting up Sunshine"
 sh /Capsule/preconfig-sunshine.sh
 
-echo "Setting up Audio"
-sh /Capsule/preconfig-audio.sh
-
 echo "Setting up XOrg"
 sh /Capsule/preconfig-xorg.sh
 
 echo "Setting up Capsule"
 sh /Capsule/preconfig-capsule.sh
+
+echo "Setting up Audio"
+sh /Capsule/preconfig-audio.sh
 
 echo "Rebooting"
 reboot
