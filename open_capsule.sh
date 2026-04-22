c5f93406331f5f0cd09fc1f1c436939d8f7c03d5
dumpstertree <dumpstertree@gmail.com>
2026-04-22 15:58:07 -0400

    Update open_capsule.sh

--- open_capsule.sh
+++ open_capsule.sh
@@ -1,20 +1,58 @@
-echo "Installing User"
-sh /Capsule/create_user.sh
+#echo "Installing User"
+#sh /Capsule/create_user.sh
+
+#echo "Installing All Dependencies"
+#sh /Capsule/install_dependencies2.sh
+
+#echo "Setting up Sunshine"
+#sh /Capsule/preconfig-sunshine.sh
+
+#echo "Setting up XOrg"
+#sh /Capsule/preconfig-xorg.sh
+
+#echo "Setting up Capsule"
+#sh /Capsule/preconfig-capsule.sh
+
+#echo "Setting up Audio"
+#sh /Capsule/preconfig-audio.sh
+
+#echo "Rebooting"
+#reboot
+
+#!/bin/bash
+set -e
+
+echo "Open how many Capsules?"
+read -r USER_COUNT
+
+if ! [[ "$USER_COUNT" =~ ^[1-9][0-9]*$ ]]; then
+    echo "Error: please enter a valid number greater than 0" >&2
+    exit 1
+fi
+
 
 echo "Installing All Dependencies"
 sh /Capsule/install_dependencies2.sh
 
-echo "Setting up Sunshine"
-sh /Capsule/preconfig-sunshine.sh
+echo "Installing Users"
+for i in $(seq 1 "$USER_COUNT"); do
 
-echo "Setting up XOrg"
-sh /Capsule/preconfig-xorg.sh
+    echo "Creating user $i of $USER_COUNT"
+    sh /Capsule/create_user.sh "gamer$i"
 
-echo "Setting up Capsule"
-sh /Capsule/preconfig-capsule.sh
+    echo "Setting up Sunshine for user $i of $USER_COUNT"
+    sh /Capsule/preconfig-sunshine.sh
+    
+    echo "Setting up Sunshine for user $i of $USER_COUNT"
+    sh /Capsule/preconfig-capsule.sh
+    
+    echo "Setting up Audio for user $i of $USER_COUNT"
+    sh /Capsule/preconfig-audio.sh
 
-echo "Setting up Audio"
-sh /Capsule/preconfig-audio.sh
+    echo "Setting up XOrg"
+    sh /Capsule/preconfig-xorg.sh
+done
 
 echo "Rebooting"
 reboot
+
