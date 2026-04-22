7b514f8c85209163addb2544c2695d8b29199850
dumpstertree <dumpstertree@gmail.com>
2026-04-22 16:04:58 -0400

    Update preconfig-capsule.sh

--- preconfig-capsule.sh
+++ preconfig-capsule.sh
@@ -1,5 +1,5 @@
 # make sure systemd can call any script in the repo
-chmod -R 777 /Capsule
+#chmod -R 777 /Capsule
 
 # copy example service into folder
 #cp /Capsule/example-capsule.service /etc/systemd/system/capsule.service
@@ -16,10 +16,10 @@ chmod -R 777 /Capsule
 # create the service target directory
 #mkdir -p /home/gamer/.config/systemd/user/default.target.wants
 #copy example host service to target
-cp /Capsule/example-capsule-host.service /etc/systemd/system/capsule-host.service
+#cp /Capsule/example-capsule-host.service /etc/systemd/system/capsule-host.service
 
 # enable now and in the future
-systemctl enable capsule-host.service
+#systemctl enable capsule-host.service
 
 #copy example user service to target
 cp /Capsule/example-capsule.service /usr/lib/systemd/user/capsule.service
